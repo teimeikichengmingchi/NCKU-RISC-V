7f00f73de23842910c0bd80c3d3a16c58db6c21a
teimeikichengmingchi <teimeikichengmingchi@gmail.com>
2022-09-24 23:40:42 -0700

    Add testing data

--- homework1/leetcode_26.c
+++ homework1/leetcode_26.c
@@ -4,11 +4,26 @@
 int removeDuplicates(int* nums, int numSize);
 
 int main(){
-    int nums[] = {0,0,0,1,1,2,3,4,4,4,5,6,7,7,7};
-    int cnt = removeDuplicates(nums, 15);
+    
+    int nums1[] = {0,0,0,1,1,2,3,4,4,4,5,6,7,7,7};
+    int cnt = removeDuplicates(nums1, 15);
     for(int i = 0; i < cnt; i++)
-        printf("%d ", nums[i]);
-       return 0;
+        printf("%d ", nums1[i]);
+    printf("\n");
+
+    int nums2[] = {-7,-7,0,1,1,3,3,4,4,4,5,5,5,6};
+    cnt = removeDuplicates(nums2, 14);
+    for(int i = 0; i < cnt; i++)
+        printf("%d ", nums2[i]);
+    printf("\n");
+
+    int nums3[] = {-1,0,0,1,1,2,3,4,4,4,5,6,7,7,7,8,9,10,11,11,11};
+    cnt = removeDuplicates(nums3, 21);
+    for(int i = 0; i < cnt; i++)
+        printf("%d ", nums3[i]);
+    printf("\n");
+    
+    return 0;
 }
 
 int removeDuplicates(int* nums, int numSize){
