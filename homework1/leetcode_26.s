a1d6c238dfaa522fc897df4d05ca3bb3511ad2c1
teimeikichengmingchi <teimeikichengmingchi@gmail.com>
2022-10-02 02:09:03 -0700

    optimized the code
    change some unnecessary instruction in for loop

--- homework1/leetcode_26.s
+++ homework1/leetcode_26.s
@@ -83,13 +83,11 @@ main:
 # removeDuplicates(int *nums, int numSize)
 removeDuplicates:
     add s0, zero, zero          # shift = 0
-    addi s1, zero, 4            # i = 1 * 4
+    addi s1, zero, 1            # i = 1 * 4
     add s2, a0, zero            # nums' base address
     addi s2, s2, 4              # nums[1]'s address
-    add s3, a1, zero
-    slli s3, s3, 2              # numSize * 4
 for:
-    bge s1, s3, forEnd
+    bge s1, a1, forEnd
 # if:
     addi t0, s2, -4             # t0 is (i - 1) * 4
     lw t1, 0(s2)                # t1 is nums[i]
@@ -102,12 +100,12 @@ else:
     sub t0, s2, s0              # t0 is the address of nums[i - shift]
     sw t1, 0(t0)
 ifEnd:
-    addi s1, s1, 4
+    addi s1, s1, 1
     addi s2, s2, 4
     j for
 forEnd:
-    sub a1, s3, s0
-    srli a1, a1, 2
+    srli s0, s0, 2
+    sub a1, a1, s0
     jr ra
 
 
@@ -117,10 +115,8 @@ forEnd:
 print:
     add t0, zero, zero          # i = 0
     add t1, a0, zero            # t1 is array's base address
-    slli t2, a1, 2              # t2 is cnt * 4, multiplied because of the performance issue
-                                # we will increase t0 by 4, so it is necessary to multiply the value of cnt by 4
 printLoop:
-    bge t0, t2, printLoopEnd
+    bge t0, a1, printLoopEnd
     lw a0, 0(t1)
     addi a7, zero, 1
     ecall
@@ -130,7 +126,7 @@ printLoop:
     addi a7, zero, 11
     ecall
     
-    addi t0, t0, 4              # i++
+    addi t0, t0, 1              # i++
     addi t1, t1, 4              # go to next element in array
     j printLoop
 printLoopEnd:
